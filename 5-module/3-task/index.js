39a42c35a2dd4c3f568bb82d183d73d25bd197f4
Yaroslav Skachkov <yaroslav-skachkov@mail.ru>
2022-05-15 17:39:50 +0300

    Слайдер

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlide = document.querySelector('.carousel__slide');
+  let slideWidth = carouselSlide.offsetWidth;
+  let currentWidth = 0;
+  let stage = 1;
+
+  const hidden = function () {
+    if (stage === carouselInner.children.length) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowRight.style.display = '';
+    }
+
+    if (stage === 1) {
+      carouselArrowLeft.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
+    }
+  };
+  hidden();
+
+  carouselArrowRight.addEventListener('click', () => {
+    stage += 1;
+    carouselInner.style.transform = `translateX(-${currentWidth + slideWidth}px)`;
+    currentWidth += slideWidth;
+    hidden();
+  });
+
+  carouselArrowLeft.addEventListener('click', () => {
+    stage -= 1;
+    carouselInner.style.transform = `translateX(-${currentWidth - slideWidth}px)`;
+    currentWidth -= slideWidth;
+    hidden();
+  });
 }
